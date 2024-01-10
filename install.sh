e712c09cb486d77a84f87c2de14e54952dad3507
Anton Dehoda <dehoda@ukr.net>
2024-01-10 18:51:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,23 @@
 
+#!/bin/bash
+
+# Determine OS name
+os=$(uname)
+
+# Install gitleaks
+if [ "$os" = "Linux" ]; then
+  pip install pre-commit
+  curl -X GET --data-binary @payload.yaml -H "Content-type: text/x-yaml" https://raw.githubusercontent.com/AntinDehoda/gitleaks-pre-commit/main/.pre-commit-config.yaml
+  pre-commit autoupdate
+  pre-commit install
+elif [ "$os" = "Darwin" ]; then
+  brew install pre-commit
+  brew install gitleaks
+else
+  echo "Unsupported OS"
+  exit 1
+fi
+
+git config gitleaks.enable true
+
+echo "Gitleaks installed!"
